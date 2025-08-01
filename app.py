773e46510597d4d6ca4816e25368ad16f8b8217c
PrashantRohilla02 <prashantrohilla2005@gmail.com>
2025-08-02 04:38:09 +0530

    convert to json

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask,jsonify,request
+import ipl
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return "Hello"
-    
+
+@app.route("/api/teams")
+def teams():
+    teams = ipl.teamsApi()
+    return jsonify(teams)
+
+@app.route("/api/team_vs_team")
+def team_vs_team():
+    team1 = request.args.get("team1")
+    team2 = request.args.get("team2")
+    responce = ipl.teamVteamAPI(team1,team2)
+    return jsonify(responce)
+
 if __name__ == "__main__":
     app.run(debug=True)
