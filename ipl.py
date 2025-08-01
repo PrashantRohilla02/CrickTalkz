e6294b959eac5dadcbb51d7eff6668a7b55bcacc
PrashantRohilla02 <prashantrohilla2005@gmail.com>
2025-08-02 04:37:32 +0530

    new api integrated

--- ipl.py
+++ ipl.py
@@ -3,4 +3,26 @@ import numpy as np
 
 matches = pd.read_csv("C:/Users/prash/CrickTalkz/DataSet/matches.csv")
 
-print(matches.head(5))
+# print(matches.head(5))
+
+def teamsApi():
+    teams = list(set(matches['team1'].unique()))
+    teams_dict = {
+        "teams":teams
+    }
+
+    return teams_dict
+
+def teamVteamAPI(team1,team2):
+    temp = matches[((matches['team1'] == team1) & (matches['team2'] == team2)) | ((matches['team1'] == team2) & (matches['team2'] == team1))]
+    total_matches = temp.shape[0]
+
+    matches_won_team1 = temp["winner"].value_counts()[team1]
+    matches_won_team2 = temp["winner"].value_counts()[team2]
+
+    comparison = {
+            "total match":total_matches,
+            "match_won_team1":int(matches_won_team1),
+            "match_won_team2":int(matches_won_team2)
+        }
+    return comparison
